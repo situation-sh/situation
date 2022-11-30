499c41efc536b522dd5451c17e5f6e725eff2a73
Alban Siffer <alban.siffer@irisa.fr>
2022-11-30 11:37:34 +0100

    add file lines functions

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -162,3 +162,38 @@ func TestFileExists(t *testing.T) {
 		t.Errorf("The file %s must exist", f)
 	}
 }
+
+func TestKeepLeaves(t *testing.T) {
+	files := []string{
+		"/d/e/f/g",
+		"/d/f",
+		"/d/e/f",
+		"/a",
+		"/a/b/c",
+		"/a/c",
+		"/b/c/d",
+		"/b",
+		"/b/c",
+		"/c",
+	}
+	expect := []string{
+		"/d/f",
+		"/d/e/f/g",
+		"/c",
+		"/b/c/d",
+		"/a/c",
+		"/a/b/c",
+	}
+	out := KeepLeaves(files)
+
+	if len(out) != len(expect) {
+		t.Errorf("output has not the right size. Expected results: %v (got %v)",
+			expect, out)
+	}
+	for i, e := range out {
+		if expect[i] != e {
+			t.Errorf("bad pruning, expect %s, got %s", expect[i], e)
+		}
+	}
+
+}
